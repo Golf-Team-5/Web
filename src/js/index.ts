f2ce636b7929d72f75d446450a7759d1a63bd653
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2019-12-09 14:12:11 +0100

    Position on course is Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,18 +1,20 @@
 import { axiosGet } from './importapi'
 //import {AxiosGetSwingData} from './position'
 import {GetScoreAndNoOfSwings} from './Score'
+import {GetHit} from './position'
 
-
+GetHit
 
 axiosGet();
 //AxiosGetSwingData();
 
 
 //element der henter par tallet til beregning af score, fra siden. 
-let ParInput: HTMLInputElement = <HTMLInputElement>document.getElementById("parInput")
+//let ParInput: HTMLInputElement = <HTMLInputElement>document.getElementById("parInput")
 
 //knap der kalder GetScoreAndNoOfSwings metoden med ParInput. 
-let scoreButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ScoreBtn");
+/* let scoreButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ScoreBtn");
 scoreButton.addEventListener("click", function() { GetScoreAndNoOfSwings(Number(ParInput.value))});
+ */
 
 
