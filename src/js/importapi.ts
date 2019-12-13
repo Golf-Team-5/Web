5c3d07ee9245debddbc357f3c33449f855e13f9d
krist1an <hrnbll@gmail.com>
2019-12-13 11:53:14 +0100

    leaderboard DONE!

--- src/js/importapi.ts
+++ src/js/importapi.ts
@@ -15,16 +15,15 @@ function axiosGet(){
     let playerScoresUri: string = "http://localhost:52549/api/swingdata/getleaderboard"
 
     axios.get<IScore[]>(playerScoresUri, {
-         params: {
-            _limit:5
-        } 
+
     })
     .then(function(response: AxiosResponse<IScore[]>) {
         // Content Area
         let leaderboardTable: HTMLTableElement = <HTMLTableElement> document.getElementById('LeaderTableModal')
         let leaderboardTable2: HTMLTableElement = <HTMLTableElement> document.getElementById('LeaderTableModal2')
         console.log(response.data)
-        
+  
+
         // Kalder her en metode som formatere inholdet fra JSON objekterne og stiller dem pænt op
         // response er JSON der kommer tilbage fra URI
         // mainLeftTable er det element vi tilføjer hvert under-element til
@@ -42,12 +41,17 @@ function addScoreToTable(res: AxiosResponse<IScore[]>, ele: HTMLTableElement){
 
    //ele.innerHTML = '<thead class="thead-dark"><tr><th colspan="3">Top 3 score</th></tr><tr><th>#</th><th>Navn</th><th>Score</th></tr></thead>'
     
-   let i: number = 5
+    
+   
+
+   let i: number = 1
 
     res.data.forEach((score: IScore) => {
 
-        if (i >= 1) {
-            let row = ele.insertRow(0)
+        
+
+        if (i <= 5) { 
+            let row = ele.insertRow(-1)
             let cell1 = row.insertCell(0)
             let cell2 = row.insertCell(1)
             let cell3 = row.insertCell(2)
@@ -55,8 +59,8 @@ function addScoreToTable(res: AxiosResponse<IScore[]>, ele: HTMLTableElement){
             cell1.innerHTML = String(i)
             cell2.innerHTML = score.playerName
             cell3.innerHTML = String(score.playerScore)
-            i -= 1
-        }
+            i += 1
+        } 
         
   
 
